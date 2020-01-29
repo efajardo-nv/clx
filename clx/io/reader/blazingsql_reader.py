c13c6245739edf4119383472ba8814a3d5a921a4
bsuryadevara <bhargavsuryadevara@gmail.com>
2020-01-29 10:12:53 -0600

    updated blazingsql reader to support join queries

--- clx/io/reader/blazingsql_reader.py
+++ clx/io/reader/blazingsql_reader.py
@@ -10,24 +10,17 @@ class BlazingSQLReader(FileReader):
         self._has_data = True
         self._bc = BlazingContext()
 
-    def fetch_data(self):
-        df = None
-        table_name = self.config["table_name"]
-        file_path = self.config["input_path"]
-        sql = self.config["sql"]
-        kwargs = self.config.copy()
-        if type in kwargs:
-            del kwargs["type"]
-        del kwargs["table_name"]
-        del kwargs["input_path"]
-
-        self._bc.create_table(table_name, file_path, **kwargs)
-
+    def fetch_data(self, sql):
+        for table in self.config['tables']:
+            table_name = table["table_name"]
+            file_path = table["input_path"]
+            kwargs = table.copy()
+            del kwargs["table_name"]
+            del kwargs["input_path"]
+            self._bc.create_table(table_name, file_path, **kwargs)
         #Query
         result = self._bc.sql(sql)
-
         self.has_data = False
-
         return result
 
     def close(self):
